ea86b60ef522c78cd2cd79a4aac69c69213d4fff
Manash111 <manashmaharjan5@gmail.com>
2025-08-07 10:01:04 +0545

    added homepage test scripts

---
+++ pages/homepage_page.py
@@ -0,0 +1,21 @@
+from base_page import BasePage
+import locators
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class HomePage(BasePage):
+
+    def verify(self):
+        expected_title = "Your store. Home page title"
+        title = self.get_title()
+        if title == expected_title:
+            print("Verified")
+
+    def navigation_top_menu(self):
+        self.click(locators.link_computer)
+        self.click(locators.link_electronics)
+        self.click(locators.link_apparel)
+        self.click(locators.link_digitalDownloads)
+        self.click(locators.link_books)
+        self.click(locators.link_giftcards)
+        print("Navigation successful")
---
+++ tests/test_homepage.py
@@ -0,0 +1,21 @@
+import unittest
+
+import locators
+from pages.homepage_page import HomePage
+from base_test import BaseTest
+
+
+class TestHomePage(BaseTest):
+    def test_homepage(self):
+        self.driver.get(locators.base_url)
+        homepage = HomePage(self.driver)
+        homepage.verify()
+        print("TC01")
+
+        #test navigation
+        homepage.navigation_top_menu()
+        print("TC02")
+
+
+if __name__ == "__main__":
+    unittest.main()
